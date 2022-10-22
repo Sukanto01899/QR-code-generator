db82cb822cebb192c1a9cb8da6f838c1ce3e9785
Sukanto Das <Suknto01899>
2022-10-22 17:45:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const bgColorPlate = document.getElementById('bg-color-set'),
+colorInput = document.querySelector('.color input[type="color"]'),
+generateBtn = document.getElementById('generate'),
+imgBox  = document.querySelector('.qr-code .img'),
+image = document.querySelector('.qr-code .img img'),
+textField = document.querySelector('.data-input textarea');
+const [ffd6cc, cce0ff, ffffcc, ccffcc, ccccff]  = document.querySelectorAll('#bg-color-set label input');
+let bgColor = 'ffffff';
+let color = '000000'
+
+bgColorPlate.addEventListener('click', ()=>{
+    if(ffd6cc.checked){
+        bgColor = 'ffd6cc'
+    }else if(cce0ff.checked){
+        bgColor = 'cce0ff'
+    }else if(ffffcc.checked){
+        bgColor = 'ffffcc'
+    }else if(ccffcc.checked){
+        bgColor = 'ccffcc'
+    }else if(ccccff.checked){
+        bgColor = 'ccccff'
+    }
+})
+
+colorInput.addEventListener('change', ()=>{
+    color = colorInput.value.substring(1)
+})
+
+const fetchRquest = async (value, color, bgColor)=>{
+    const url = `https://api.qrserver.com/v1/create-qr-code/?data=${value}&size=200x200&color=${color}&bgcolor=${bgColor}&margin=10`
+    const data = await fetch(url);
+    generateBtn.innerText= 'Generate';
+    generateBtn.removeAttribute('disabled')
+    imgBox.classList.remove('loading')
+    image.src = data.url
+};
+
+generateBtn.addEventListener('click', ()=>{
+    if(!textField.value) return alert('Give something')
+    generateBtn.innerText= 'Generating...';
+    generateBtn.setAttribute('disabled', true) 
+    imgBox.classList.add('loading')
+    fetchRquest(textField.value, color, bgColor)
+})
